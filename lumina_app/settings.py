4a0e84e842d0286b5a7a0d6bc7e7331b4b38a7f6
dzakyadlh <106021129+dzakyadlh@users.noreply.github.com>
2024-12-04 00:08:32 +0700

    updates for deployment to render

--- lumina_app/settings.py
+++ lumina_app/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tkmbrg_21_c%b$2aym5c2mj_o161-0#lw74q3qu7gmqxrk)lrn"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -79,7 +79,8 @@ TEMPLATES = [
 WSGI_APPLICATION = "lumina_app.wsgi.application"
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',  # Allow React frontend
+    'https://lumina-fe-indol.vercel.app',
+    'http://localhost:3000'
 ]
 
 # Database
